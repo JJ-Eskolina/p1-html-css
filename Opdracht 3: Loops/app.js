a581071d28b81fc1324ba1d975511f64127bb8de
JJ-escobar <je.eskolina@gmail.com>
2024-12-02 11:58:18 +0100

    .

---
+++ Opdracht 3: Loops/app.js
@@ -0,0 +1,9 @@
+
+for (let i = 0; i <= 100; i++){
+    document.getElementById("output1").innerHTML += i + "<br>";
+}
+
+let output = document.getElementById("output2");
+        for (let i = 100; i >= 0; i--) {
+            output.innerHTML += i + "<br>"; 
+        }
